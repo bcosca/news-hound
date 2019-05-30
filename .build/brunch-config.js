5e257e5e8f90434d067e50a2d46965ec5b3c71f9
Bong Cosca <bong.cosca@gmail.com>
2019-05-30 12:46:47 +0000

    Add brunch configuration

---
+++ .build/brunch-config.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function() {
+
+  const SRC='src/',
+        DST='../pub/';
+
+	module.exports={
+		plugins:{
+      eslint:{
+        pattern:/\.js$/
+      },
+      terser:{
+        mangle:false
+      }
+		},
+		paths:{
+			public:DST,
+			watched:[ SRC ]
+		},
+		files:{
+			javascripts:{
+				joinTo:{ 'js/app.min.js':[ SRC+'tags/**/*',SRC+'js/*' ] },
+        order:{ after:SRC+'js/_boot.js' }
+			}
+		},
+		modules:{
+			wrapper:false,
+			definition:false
+		},
+		conventions: {
+			ignored:function() {
+				return false;
+			}
+		},
+		npm:{ enabled:false },
+		notifications:false,
+		optimize:true
+	};
+
+})();
