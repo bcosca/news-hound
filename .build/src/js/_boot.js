1761c028076afe2c477673b0570b395c5977a94b
Bong Cosca <bong.cosca@gmail.com>
2019-05-30 12:21:34 +0000

    First public release

---
+++ .build/src/js/_boot.js
@@ -0,0 +1,17 @@
+/* global roadshow */
+
+'use strict';
+
+(function() {
+
+  roadshow.load({
+    app:{
+      brand:'News Hound'
+    },
+    theme:{
+      primary:'red-700',
+      primaryAlt:'red-a700'
+    }
+  });
+
+})();
